dd60c5af9fe62b50777d9e020fcd40aa57680c0c
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-08-10 09:35:30 +0530

    Time: 34 ms (73.12%), Space: 7.8 MB (99.56%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,34 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    bool winnerSquareGame(int n) {
+       vector<bool> dp(n+1);
+       for(int i =1;i<=n;i++) for(int s=1;s*s<=i;s++)if(!dp[i-s*s]){dp[i]=true; break;}
+       return dp[n]; 
+    }
+};
