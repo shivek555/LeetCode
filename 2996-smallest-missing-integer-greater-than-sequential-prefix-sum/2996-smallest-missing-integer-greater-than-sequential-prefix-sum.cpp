24923774484ef0b6b1508b37e4bdedf98f63bc98
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-08-11 12:34:20 +0530

    Time: 2 ms (29.49%), Space: 22.3 MB (99.83%) - LeetHub

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -0,0 +1,47 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    int missingInteger(vector<int>& nums) {
+        unordered_set<int> values;
+        for (int i : nums) {
+            values.insert(i);
+        }
+        int currSum = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] - 1 == nums[i - 1]) {
+                currSum += nums[i];
+            } else {
+                break;
+            }
+        }
+        while (values.find(currSum) != values.end()) {
+            currSum++;
+        }
+        return currSum;
+    }
+};
