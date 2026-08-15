7ed9d39fa1979073c3db5962b5f542f43d444f30
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-08-15 13:12:41 +0530

    Time: 46 ms (49.04%), Space: 55.3 MB (48.08%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.ts
@@ -0,0 +1,17 @@
+type Func<T, U> = (...args: T[]) => U;
+
+function once<T, U>(fn: Func<T, U>): Func<T, U | undefined> {
+  let hasBeenCalled = false;
+  let result: U;
+
+  return function (...args: T[]): U | undefined {
+    if (!hasBeenCalled) {
+      result = fn(...args);
+      hasBeenCalled = true;
+      return result;
+    } else {
+      return undefined;
+    }
+  }
+}
+
