8f30d8152f0c494d6de04bcc67d2b98ca68686c5
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-08-15 13:10:28 +0530

    Time: 54 ms (35.86%), Space: 57.9 MB (61.98%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.ts
@@ -0,0 +1,19 @@
+type ReturnObj = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+function createCounter(init: number): ReturnObj {
+    let cnt = init;
+    return{
+        increment: () => cnt+=1, decrement: () => cnt-=1, reset: () => (cnt=init),
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
