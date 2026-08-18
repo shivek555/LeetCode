86649583dc5b09cb19e1d4c50fbab576c55c7959
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-08-18 19:57:25 +0530

    Time: 0 ms (100%), Space: 30.8 MB (99.93%) - LeetHub

---
+++ 4020-elevator-requests-i/4020-elevator-requests-i.cpp
@@ -0,0 +1,38 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    int elevatorRequests(int n, vector<int>& requests) {
+        return requests[0] + ranges::fold_left(
+            requests | views::pairwise_transform([](int a, int b) { 
+                return abs(b - a); 
+            }),
+            0, 
+            plus{}
+        );
+    }
+};
