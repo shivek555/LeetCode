c77058fe4db4aa8e2dc60d4f3fb645ec303ecaf4
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-08-24 14:39:44 +0530

    Time: 4 ms (60.87%), Space: 8.3 MB (99.95%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -0,0 +1,45 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    int repeatedStringMatch(string a, string b) {
+        int A = 1;
+        string c = a;
+
+        for (; c.length() < b.length(); A++)
+            c += a;
+        if (c.find(b) != string::npos)
+            return A;
+
+        c += a;
+
+        if (c.find(b) != string::npos)
+            return A + 1;
+
+        return -1;
+    }
+};
