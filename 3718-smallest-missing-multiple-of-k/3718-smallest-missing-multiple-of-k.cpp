81480a764081645968bce058d6ccd09072913951
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-08-25 09:38:17 +0530

    Time: 0 ms (100%), Space: 23.7 MB (99.84%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,42 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    int missingMultiple(vector<int>& nums, int k) {
+        std::sort(nums.begin(), nums.end());
+
+        int search = k;
+        for (const int& num : nums) {
+            if (num == search)
+                search += k;
+            else if (num > search)
+                return search;
+        }
+
+        return search;
+    }
+};
