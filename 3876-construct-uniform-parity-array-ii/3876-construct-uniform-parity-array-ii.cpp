dc3ffa821cad12f9a8fd09e0bc6c3cec338a3e29
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-09-03 10:45:22 +0530

    Time: 0 ms (100%), Space: 162 MB (100%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,45 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    bool uniformArray(vector<int>& nums1) {
+      int mn = nums1[0];
+        bool hasOdd = false;
+        for (int v : nums1) {
+            if (v < mn) {
+                mn = v;
+            }
+            if (v & 1) {
+                hasOdd = true;
+            }
+        }
+        if (mn & 1) {
+            return true;
+        }
+        return !hasOdd;  
+    }
+};
