9a8a6d86c1e8f018b588005fafbf15b652851a91
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-09-06 15:20:36 +0530

    Time: 0 ms (100%), Space: 7.8 MB (81.41%) - LeetHub

---
+++ 0754-reach-a-number/0754-reach-a-number.cpp
@@ -0,0 +1,36 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    int reachNumber(int target) {
+        target = abs(target);
+        int k = 0;
+        while (target > 0)
+            target -= ++k;
+        return target % 2 == 0 ? k : k + 1 + k%2;
+    }
+};
