bff411e81fe89980d7bf8c33a744295da008dd8c
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-09-06 15:07:28 +0530

    Time: 156 ms (46.25%), Space: 94.7 MB (10.8%) - LeetHub

---
+++ 0752-open-the-lock/0752-open-the-lock.cpp
@@ -0,0 +1,57 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[256 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    int openLock(vector<string>& deadends, string target) {
+        unordered_set<string> deadSet(deadends.begin(), deadends.end());
+        if (deadSet.count("0000")) return -1;
+        queue<string> q({"0000"});
+        for (int steps = 0; !q.empty(); ++steps) {
+            for (int i = q.size(); i > 0; --i) {
+                auto curr = q.front(); q.pop();
+                if (curr == target) return steps;
+                for (auto nei : neighbors(curr)) {
+                    if (deadSet.count(nei)) continue;
+                    deadSet.insert(nei); // Marked as visited
+                    q.push(nei);
+                }
+            }
+        }
+        return -1;
+    }
+    vector<string> neighbors(const string& code) {
+        vector<string> result;
+        for (int i = 0; i < 4; i++) {
+            for (int diff = -1; diff <= 1; diff += 2) {
+                string nei = code;
+                nei[i] = (nei[i] - '0' + diff + 10) % 10 + '0';
+                result.push_back(nei);
+            }
+        }
+        return result;
+    }
+};
