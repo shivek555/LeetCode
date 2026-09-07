1a829f411fa3534e2e41cf5734646594988a0304
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-09-07 16:44:47 +0530

    Time: 0 ms (100%), Space: 84 MB (100%) - LeetHub

---
+++ 0775-global-and-local-inversions/0775-global-and-local-inversions.cpp
@@ -0,0 +1,37 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    bool isIdealPermutation(vector<int>& nums) {
+        int cmax=0,n=nums.size();
+        for(int i =0;i<n-2;++i){
+            cmax=max(cmax,nums[i]);
+            if(cmax>nums[i+2]) return false;
+        }
+        return true;
+    }
+};
