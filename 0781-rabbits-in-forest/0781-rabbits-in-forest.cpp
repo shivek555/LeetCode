e6b94e266aeffe6e4ceac161fd1ef86ea421f128
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-09-07 20:52:38 +0530

    Time: 0 ms (100%), Space: 11.5 MB (99.92%) - LeetHub

---
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.cpp
@@ -0,0 +1,37 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    int numRabbits(vector<int>& nums, int total =0) {
+         unordered_map <int,int> mpp;
+        for (int i : nums) mpp[i]++;
+
+        for (auto& p : mpp)
+        total += ceil((double)p.second / (p.first + 1)) * (p.first + 1);
+        return total;
+    }
+};
