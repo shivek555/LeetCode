385980033c056f9c6758fec02f45d5d1bd62f140
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-09-07 21:04:23 +0530

    Time: 0 ms (100%), Space: 13.1 MB (100%) - LeetHub

---
+++ 0789-escape-the-ghosts/0789-escape-the-ghosts.cpp
@@ -0,0 +1,36 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    bool escapeGhosts(vector<vector<int>>& ghosts, vector<int>& t) {
+        int d = abs(t[0]) + abs(t[1]);
+        for(auto g: ghosts)
+            if (d >= abs(t[0] - g[0]) + abs(t[1] - g[1]))
+                return false;
+        return true;
+    }
+};
