9ff4d8dd38ac126f5a1717e3ebc79620d6d78dbf
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-09-09 21:34:08 +0530

    Time: 4 ms (63.69%), Space: 8.5 MB (85.71%) - LeetHub

---
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -0,0 +1,45 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    double champagneTower(int poured, int query_row, int query_glass) {
+        double tower[102][102] = {0.0};
+        tower[0][0] = (double)poured;
+        
+        for (int r = 0; r <= query_row; r++) {
+            for (int c = 0; c <= r; c++) {
+                if (tower[r][c] > 1.0) {
+                    double excess = (tower[r][c] - 1.0) / 2.0;
+                    tower[r][c] = 1.0;
+                    tower[r + 1][c] += excess;
+                    tower[r + 1][c + 1] += excess;
+                }
+            }
+        }
+        return tower[query_row][query_glass];
+    }
+};
