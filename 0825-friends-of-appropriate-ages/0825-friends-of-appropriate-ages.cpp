e1969a59b473009803f1514bce5e08b1e6e423cb
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-09-12 13:14:01 +0530

    Time: 8 ms (50.83%), Space: 39.7 MB (100%) - LeetHub

---
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.cpp
@@ -0,0 +1,44 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+        int numFriendRequests(vector<int>& ages) {
+        unordered_map<int, int> count;
+        for (int &age : ages)
+            count[age]++;
+        int res = 0;
+        for (auto &a : count)
+            for (auto &b : count)
+                if (request(a.first, b.first))
+                    res += a.second * (b.second - (a.first == b.first ? 1 : 0));
+        return res;
+    }
+
+    bool request(int a, int b) {
+        return !(b <= 0.5 * a + 7 || b > a || (b > 100 && a < 100));
+    }
+};
