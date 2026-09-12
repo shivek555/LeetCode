d59978632369362be4a114cbf9a77dddc3a5ba4b
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-09-12 13:04:36 +0530

    Time: 31 ms (22.58%), Space: 16.4 MB (9.27%) - LeetHub

---
+++ 0823-binary-trees-with-factors/0823-binary-trees-with-factors.cpp
@@ -0,0 +1,42 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+        int numFactoredBinaryTrees(vector<int>& A) {
+        long res = 0, mod = pow(10, 9) + 7;
+        sort(A.begin(), A.end());
+        unordered_map<int, long> dp;
+        for (int i = 0; i < A.size(); ++i) {
+            dp[A[i]] = 1;
+            for (int j = 0; j < i; ++j)
+                if (A[i] % A[j] == 0)
+                    dp[A[i]] = (dp[A[i]] + dp[A[j]] * dp[A[i] / A[j]]) % mod;
+            res = (res + dp[A[i]]) % mod;
+        }
+        return res;
+    }
+};
