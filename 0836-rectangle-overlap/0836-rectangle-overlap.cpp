4b480e5db7b272bdd37a0e446aa4fbd206178705
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-09-14 09:59:31 +0530

    Time: 0 ms (100%), Space: 9.5 MB (100%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -0,0 +1,40 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        if (rec1[2] <= rec2[0])
+            return false;
+        if (rec1[0] >= rec2[2])
+            return false;
+        if (rec1[3] <= rec2[1])
+            return false;
+        if (rec1[1] >= rec2[3])
+            return false;
+        return true;
+    }
+};
