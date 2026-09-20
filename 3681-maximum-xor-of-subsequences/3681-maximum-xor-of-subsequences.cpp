1a7f36fbac4d45baf1c0e5fcc36a3e16eae6ef15
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-09-20 12:26:03 +0530

    Time: 61 ms (68.87%), Space: 136.3 MB (100%) - LeetHub

---
+++ 3681-maximum-xor-of-subsequences/3681-maximum-xor-of-subsequences.cpp
@@ -0,0 +1,48 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    int maxXorSubsequences(vector<int>& nums) {
+        vector<int> temp(32, 0);
+        for (int num : nums) {
+            for (int i = 31; i >= 0; i--) {
+                if (!(num & (1 << i))) continue;
+                if (!temp[i]) {
+                    temp[i] = num;
+                    break;
+                }
+                num ^= temp[i];
+            }
+        }
+
+        int ans = 0;
+        for (int i = 31; i >= 0; i--) {
+            ans = max(ans, ans ^ temp[i]);
+        }
+        return ans;
+    }
+};
