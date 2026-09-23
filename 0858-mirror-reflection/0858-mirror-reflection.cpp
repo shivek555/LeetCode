89aa0487e013e1f8be9c3ce3333809a65f2dbb87
shivek555 <68592628+shivek555@users.noreply.github.com>
2026-09-23 12:11:24 +0530

    Time: 0 ms (100%), Space: 7.8 MB (81.44%) - LeetHub

---
+++ 0858-mirror-reflection/0858-mirror-reflection.cpp
@@ -0,0 +1,46 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    int gcd(int a, int b) {
+        while(b) {
+            a = a % b;
+            swap(a, b);
+        }
+        return a;
+    }
+    
+    int mirrorReflection(int p, int q) {
+        int lcm = (p*q)/gcd(p, q); 
+        int m = lcm/p;
+        int n = lcm/q;
+        if(m%2==0 && n%2==1) return 0;
+        if(m%2==1 && n%2==1) return 1;
+        if(m%2==1 && n%2==0) return 2;
+        return -1;
+    }
+};
